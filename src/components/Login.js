26f6c218b6726935cc2efe6ac7658bda433285ef
ProMehedi <mehedibd563@gmail.com>
2021-05-29 14:33:40 +0600

    Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div id='login-page'>
+      <div id='login-card'>
+        <h2>Welcome to Unichat!</h2>
+        <button className='login-button google'>
+          <GoogleOutlined /> Sign In With Google
+        </button>
+        <button className='login-button facebook'>
+          <FacebookOutlined /> Sign In With Facebook
+        </button>
+      </div>
     </div>
   )
 }

116b65261125d9f1e2ccb21b66c1f091be5bf779
ProMehedi <mehedibd563@gmail.com>
2021-05-29 15:48:06 +0600

    AuthContext Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-// import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from '../contexts/AuthContext'
 
 import Chats from './Chats'
 import Login from './Login'
@@ -11,12 +10,12 @@ function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
       <Router>
-        {/* <AuthProvider> */}
-        <Switch>
-          <Route path='/chats' component={Chats} />
-          <Route path='/' component={Login} />
-        </Switch>
-        {/* </AuthProvider> */}
+        <AuthProvider>
+          <Switch>
+            <Route path='/chats' component={Chats} />
+            <Route path='/' component={Login} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   )

11748127581121dfa7842abefd05ccba46f3b313
ProMehedi <mehedibd563@gmail.com>
2021-05-29 14:49:03 +0600

    Setup Firebase Auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons'
 
+import firebase from 'firebase/app'
+import { auth } from '../firebase'
+
 const Login = () => {
   return (
     <div id='login-page'>
       <div id='login-card'>
         <h2>Welcome to Unichat!</h2>
-        <button className='login-button google'>
+        <button
+          className='login-button google'
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+          }
+        >
           <GoogleOutlined /> Sign In With Google
         </button>
-        <button className='login-button facebook'>
+        <button
+          className='login-button facebook'
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+          }
+        >
           <FacebookOutlined /> Sign In With Facebook
         </button>
       </div>
